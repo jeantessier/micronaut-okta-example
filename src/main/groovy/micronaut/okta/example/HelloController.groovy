48d66be00c18378c71acd0b0486bc0ad5ee8daab
Jean Tessier <jean@jeantessier.com>
2021-01-09 13:23:32 -0800

    Fix signature in controller action
    Apparently, the security integration needs an explicitly typed
    `principal` parameter.

--- src/main/groovy/micronaut/okta/example/HelloController.groovy
+++ src/main/groovy/micronaut/okta/example/HelloController.groovy
@@ -5,14 +5,16 @@ import io.micronaut.http.annotation.Get
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
 
+import java.security.Principal
+
 @Controller("/hello")
 class HelloController {
 
     @Get
     @Secured(SecurityRule.IS_AUTHENTICATED)
-    def hello(principal) {
+    def hello(Principal principal) {
         [
-                message: "Hello, ${principal.name}!",
+                message: "Hello, ${principal.name}!" as String,
                 principal: principal.name,
         ]
     }
