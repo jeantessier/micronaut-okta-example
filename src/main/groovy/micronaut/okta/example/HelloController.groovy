0af67c90d89c42dae48a3def687c8bb6b5492885
Jean Tessier <jean@jeantessier.com>
2022-04-04 19:07:14 -0700

    Add logging
    Dig past the `Principal` interface to get at implementation details.

--- src/main/groovy/micronaut/okta/example/HelloController.groovy
+++ src/main/groovy/micronaut/okta/example/HelloController.groovy
@@ -4,18 +4,23 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
+import org.slf4j.LoggerFactory
 
 import java.security.Principal
 
 @Controller("/hello")
 class HelloController {
 
+    static final LOGGER = LoggerFactory.getLogger(this)
+
     @Get
     @Secured(SecurityRule.IS_AUTHENTICATED)
     def hello(Principal principal) {
+        LOGGER.info("principal: ${principal} (${principal.getClass().name})")
         [
-                message: "Hello, ${principal.name}!" as String,
+                message: "Hello, ${principal.attributes.given_name}!" as String,
                 principal: principal.name,
+                principalJson: principal.toJson(),
         ]
     }
 
